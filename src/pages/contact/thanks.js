3ad555551e354fcc1dff7964d96fe89f65df1cc6
Isanderr <isander@wp.pl>
2019-10-13 18:25:03 +0200

    add local repo

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React from "react";
+import Layout from "../../components/Layout";
 
 export default () => (
-  <Layout>
+  <div>
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Dziękuje za wiadomość!</h1>
+          <p>
+            Postaram się udzielić odpowiedzi tak szybko jak tylko to będzie
+            możliwe.
+          </p>
         </div>
       </div>
     </section>
   </Layout>
-)
+);
