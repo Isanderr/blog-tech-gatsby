ae1224a0b2a0c9b97dbc6bc14b4713fd3f360402
Isanderr <isander@wp.pl>
2019-10-16 19:21:47 +0200

    add some links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
 import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram.svg";
+import linkedin from "../img/social/linkedin";
 import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
+// import vimeo from "../img/social/vimeo.svg";
 
 const Footer = class extends React.Component {
   render() {
@@ -74,14 +74,14 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://www.facebook.com/isandervod">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://twitter.com/IsanderVOD">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -89,20 +89,23 @@ const Footer = class extends React.Component {
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a
+                  title="linkedin"
+                  href="https://www.linkedin.com/in/%C5%82ukasz-cendrowski-5a45b5132/"
+                >
                   <img
-                    src={instagram}
-                    alt="Instagram"
+                    src={linkedin}
+                    alt="Linkedin"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="vimeo" href="https://vimeo.com">
+                {/* <a title="vimeo" href="https://vimeo.com">
                   <img
                     src={vimeo}
                     alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
