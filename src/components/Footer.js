9a219082c2b3848f9245a743991767e864095ee3
Isanderr <isander@wp.pl>
2019-10-16 19:29:53 +0200

    add some links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
 import facebook from "../img/social/facebook.svg";
-import linkedin from "../img/social/linkedin";
+import linkedin from "../img/social/linkedin.svg";
 import twitter from "../img/social/twitter.svg";
 // import vimeo from "../img/social/vimeo.svg";
 
